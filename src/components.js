bd7caf48ed07ada9f2f72b7497598a5d5ebb3683
Aron Kuch <AronKuch@gmail.com>
2021-11-26 11:57:39 -0500

    Added basic SectionData area

--- src/components.js
+++ src/components.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import ReactHtmlParser from "react-html-parser";
+
 export const Settings = () => {
   return <div className="settings">Settings</div>;
 };
@@ -6,6 +9,22 @@ export const When = () => {
   return <div className="when">Date -- Time</div>;
 };
 
+const SectionData = (props) => {
+  let returnHTML = "";
+  for (let i = 0; i <= props.number; i++) {
+    returnHTML += `<li key=${i}>${props.name} data will go here ${i}</li>`;
+  }
+  return ReactHtmlParser(returnHTML);
+};
+
 export const Section = (props) => {
-  return <div className="{props.name}">{props.name} +</div>;
+  let [number, setStatus] = useState(0);
+  return (
+    <div className="{props.name}">
+      <h1 onClick={() => setStatus(++number)}>{props.name} +</h1>
+      <ul>
+        <SectionData name={props.name} number={number} />
+      </ul>
+    </div>
+  );
 };
